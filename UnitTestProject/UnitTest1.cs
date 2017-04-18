ac0c2b7eaceab8cf70a589c3620adcbbe8eedbdf
guillaumeLis <g.liscouet@sfr.fr>
2017-04-18 11:37:21 +0200

    modify

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -2,7 +2,6 @@
 //using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JenkinsConsole;
 using NUnit.Framework;
-using NUnit.Framework.SyntaxHelpers;
 
 namespace UnitTestProject
 {
@@ -15,7 +14,7 @@ namespace UnitTestProject
             //Test d'addition
             Program prog = new Program();
             int result = prog.Add(5, 4);
-            Assert.That(result, Is.EqualTo(9));
+            Assert.That(result, NUnit.Framework.SyntaxHelpers.Is.EqualTo(9));
         }
 
         [Test]
@@ -23,7 +22,7 @@ namespace UnitTestProject
         {
             Program prog = new Program();
             float result = prog.Division(4, 2);
-            Assert.That(result, Is.EqualTo(2));
+            Assert.That(result, NUnit.Framework.SyntaxHelpers.Is.EqualTo(2));
         }
 
         [Test]
@@ -31,7 +30,7 @@ namespace UnitTestProject
         {
             Program prog = new Program();
             int result = prog.Soustraction(4, 3);
-            Assert.That(result, Is.EqualTo(1));
+            Assert.That(result, NUnit.Framework.SyntaxHelpers.Is.EqualTo(1));
         }
 
     }
